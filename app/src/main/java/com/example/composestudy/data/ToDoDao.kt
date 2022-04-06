dd3249a9b99d32425ec9b7cbfdf5d7f0aac1cd91
kyusikkyu <kyusikkyu@gmail.com>
2022-04-06 15:11:36 +0900

    roomRx -> coroutine

--- app/src/main/java/com/example/composestudy/data/ToDoDao.kt
+++ app/src/main/java/com/example/composestudy/data/ToDoDao.kt
@@ -2,36 +2,35 @@ package com.example.composestudy.data
 
 import androidx.room.*
 import com.example.composestudy.data.models.ToDoTask
-import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ToDoDao {
 
     @Query("SELECT * FROM todo_table ORDER BY id ASC")
-    fun getAllTasks(): Single<List<ToDoTask>>
+    fun getAllTasks(): Flow<List<ToDoTask>>
 
     @Query("SELECT * FROM todo_table WHERE id=:taskId")
-    fun getSelectedTask(taskId: Int): Single<ToDoTask>
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun addTask(toDoTask: ToDoTask): Completable
+    fun addTask(toDoTask: ToDoTask)
 
     @Update
-    fun updateTask(toDoTask: ToDoTask): Completable
+    fun updateTask(toDoTask: ToDoTask)
 
     @Delete
-    fun deleteTask(toDoTask: ToDoTask): Completable
+    fun deleteTask(toDoTask: ToDoTask)
 
     @Query("DELETE FROM todo_table")
-    fun deleteAllTasks(): Completable
+    fun deleteAllTasks()
 
     @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
-    fun searchDatabase(searchQuery: String): Single<List<ToDoTask>>
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
 
     @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
-    fun sortByLowPriority(): Single<List<ToDoTask>>
+    fun sortByLowPriority(): Flow<List<ToDoTask>>
 
     @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
-    fun sortByHighPriority(): Single<List<ToDoTask>>
+    fun sortByHighPriority(): Flow<List<ToDoTask>>
 }
